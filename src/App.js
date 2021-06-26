ea71de1938670e2df48f398d2507572402773daf
Sean Oyler <xrysen@gmail.com>
2021-06-26 09:08:48 -0700

    Loadings basic ArWing model

--- src/App.js
+++ src/App.js
@@ -1,9 +1,49 @@
-import './App.css';
-import React from "react";
-import { Canvas } from "react-three-fiber";
+import "./App.css";
+import React, { Suspense } from "react";
+import { Canvas, useLoader } from "react-three-fiber";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+
+function Loading() {
+  return (
+    <mesh visible position={[0, 0, 0]} rotation={[0, 0, 0]}>
+      <sphereGeometry attach="geometry" args={[1, 16, 16]} />
+      <meshStandardMaterial
+        attach="material"
+        color="white"
+        transparent
+        opacity={0.6}
+        roughness={1}
+        metalness={0}
+      />
+    </mesh>
+  );
+}
+
+function ArWing() {
+  const { nodes } = useLoader(GLTFLoader, "models/arwing.glb");
+  return (
+    <group>
+      <mesh visible geometry={nodes.Default.geometry}>
+        <meshStandardMaterial
+          attach="material"
+          color="white"
+          roughness={0.3}
+          metalness={0.3}
+        />
+      </mesh>
+    </group>
+  );
+}
 
 function App() {
- return <Canvas style = {{ background: "#171717" }}></Canvas>
+  return (
+    <Canvas style={{ background: "#171717" }}>
+      <directionalLight intensity={0.5} />
+      <Suspense fallback={<Loading />}>
+        <ArWing />
+      </Suspense>
+    </Canvas>
+  );
 }
 
 export default App;
