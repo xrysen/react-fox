eada77402e82964cf6ce09507623777f119c8913
Sean Oyler <xrysen@gmail.com>
2021-06-26 16:43:42 -0700

    Adds moving terrain

--- src/App.js
+++ src/App.js
@@ -69,6 +69,34 @@ const CameraControls = () => {
   );
 };
 
+const GROUND_HEIGHT = -50;
+
+const Terrain = () => {
+  const terrain = useRef();
+
+  useFrame(() => {
+    terrain.current.position.z += 0.4;
+  });
+
+  return (
+    <mesh
+      visible
+      position={[0, GROUND_HEIGHT, 0]}
+      rotation={[-Math.PI / 2, 0, 0]}
+      ref={terrain}
+    >
+      <planeBufferGeometry attach = "geometry" args={[5000, 5000, 128, 128]} />
+      <meshStandardMaterial
+        attach = "material"
+        color="white"
+        roughness={1}
+        metalness={0}
+        wireframe
+        />
+    </mesh>
+  )
+}
+
 function App() {
   return (
     <>
@@ -78,6 +106,7 @@ function App() {
         <Suspense fallback={<Loading />}>
           <ArWing />
         </Suspense>
+        <Terrain />
       </Canvas>
     </>
   );
