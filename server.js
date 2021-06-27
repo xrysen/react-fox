cb914229b7fa3f8b43478f02cca414a073fc2aa5
Sean Oyler <xrysen@gmail.com>
2021-06-27 11:48:34 -0700

    Cylander fun

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require("cors");
 const app = express();
 const path = require("path");
 const PORT = 8000;
 
 const dir = path.join(__dirname, 'public');
 app.use(express.static(dir));
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'));
