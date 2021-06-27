3c23ca4748e3e050c468140287699896fee5e5fa
Sean Oyler <xrysen@gmail.com>
2021-06-26 20:34:53 -0700

    Adds fog so black hole isn't as pronounced

--- public/app.js
+++ public/app.js
@@ -1,8 +1,11 @@
+
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer({ antialias: true});
 
-
+renderer.setClearColor(0x000022);
+scene.fog = new THREE.FogExp2(0x000022, 0.00225);
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 camera.position.z = 5;
