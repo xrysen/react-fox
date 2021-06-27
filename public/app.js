eacc4d5f289242df3b1b7e9852f7746780fffcfd
Sean Oyler <xrysen@gmail.com>
2021-06-26 20:29:42 -0700

    Adds spacey background

--- public/app.js
+++ public/app.js
@@ -9,17 +9,11 @@ camera.position.z = 5;
 
 const loader = new THREE.TextureLoader();
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ map: loader.load("http://localhost:8000/images/space.jpeg")})
+const geometry = new THREE.CylinderGeometry(100, 100, 5000, 24, 24, true);
+const material = new THREE.MeshBasicMaterial({ map: loader.load("http://localhost:8000/images/space.jpeg"), side: THREE.BackSide })
 const cube = new THREE.Mesh ( geometry, material);
+cube.rotation.x = -Math.PI / 2;
 scene.add(cube);
-
-const geometry2 = new THREE.BoxGeometry(3, 3, 3);
-const material2 = new THREE.MeshBasicMaterial({
-  color: "#dadada", wireframe: true, transparent: true
-});
-const wireFrameCube = new THREE.Mesh(geometry2, material2);
-scene.add(wireFrameCube);
 
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
 scene.add(ambientLight);
@@ -30,10 +24,8 @@ scene.add(pointLight);
 
 const animate = () => {
   requestAnimationFrame(animate);
-  cube.rotation.x += 0.04;
-  cube.rotation.y += 0.04;
-  wireFrameCube.rotation.x -= 0.01;
-  wireFrameCube.rotation.y -= 0.01;
+  // cube.rotation.x += 0.04;
+  // cube.rotation.y += 0.04;
   renderer.render(scene, camera);
 }
 
